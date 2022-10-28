8ec500d82ab5490f649419f2242a5f9acddbff03
Devon Steenberg <devonsteenberg@hotmail.com>
2022-10-29 09:27:15 +1300

    Improve quicksort

--- src/lists/sorting/quicksort.rs
+++ src/lists/sorting/quicksort.rs
@@ -3,68 +3,61 @@ pub fn quicksort<T: PartialOrd>(vec: &mut [T]) {
         return;
     }
 
-    _quicksort(vec, 0, vec.len() - 1);
+    _quicksort(vec);
 }
 
-fn _quicksort<T: PartialOrd>(vec: &mut [T], lower: usize, upper: usize) {
-    let pivot_index = first_pivot_index(vec, lower, upper);
-    let partition_index = partition(vec, pivot_index, lower, upper);
-    if partition_index > lower {
-        _quicksort(vec, lower, partition_index - 1);
+fn _quicksort<T: PartialOrd>(vec: &mut [T]) {
+    let vec_len = vec.len();
+    if vec_len < 2 {
+        return;
     }
-    if partition_index < upper {
-        _quicksort(vec, partition_index + 1, upper);
-    }
+
+    let pivot_index = first_pivot_index(vec);
+    let partition_index = partition(vec, pivot_index);
+    _quicksort(&mut vec[0..partition_index]);
+    _quicksort(&mut vec[partition_index + 1..vec_len]);
 }
 
-fn first_pivot_index<T>(_vec: &mut [T], lower: usize, _upper: usize) -> usize {
-    return lower;
+fn first_pivot_index<T>(_vec: &mut [T]) -> usize {
+    return 0;
 }
 
-fn middle_pivot_index<T>(_vec: &mut [T], lower: usize, upper: usize) -> usize {
-    let middle_float = (upper + lower) as f64 / 2.0;
+#[allow(dead_code)]
+fn middle_pivot_index<T>(vec: &mut [T]) -> usize {
+    let middle_float = (vec.len()) as f64 / 2.0;
     return middle_float.round() as usize;
 }
 
-fn last_pivot_index<T>(_vec: &mut [T], _lower: usize, upper: usize) -> usize {
-    return upper;
+#[allow(dead_code)]
+fn last_pivot_index<T>(vec: &mut [T]) -> usize {
+    return vec.len() - 1;
 }
 
-fn partition<T: PartialOrd>(
-    vec: &mut [T],
-    pivot_index: usize,
-    lower: usize,
-    upper: usize,
-) -> usize {
-    if lower >= upper {
-        // FIXME what do I return here?
-        return lower;
-    }
+fn partition<T: PartialOrd>(vec: &mut [T], pivot_index: usize) -> usize {
+    let last_index = vec.len() - 1;
+    vec.swap(pivot_index, last_index);
 
-    vec.swap(pivot_index, upper);
-
-    let mut division_index = lower;
-    for i in lower..upper {
-        if vec[i] < vec[upper] {
+    let mut division_index = 0;
+    for i in 0..last_index {
+        if vec[i] < vec[last_index] {
             vec.swap(i, division_index);
             division_index += 1;
         }
     }
 
-    vec.swap(upper, division_index);
+    vec.swap(last_index, division_index);
 
     division_index
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::quicksort::{partition, quicksort};
+    use super::*;
 
     #[test]
     fn test_partition_1() {
         let mut vec = vec![5, 7, 8, 2];
-        let vec_len = vec.len() - 1;
-        let partition_index = partition(&mut vec, 0, 0, vec_len);
+        let partition_index = partition(&mut vec, 0);
         assert_eq!(vec, [2, 5, 8, 7]);
         assert_eq!(partition_index, 1);
     }
@@ -72,8 +65,7 @@ mod tests {
     #[test]
     fn test_partition_2() {
         let mut vec = vec![5, 7, 8, 2];
-        let vec_len = vec.len() - 1;
-        let partition_index = partition(&mut vec, 1, 0, vec_len);
+        let partition_index = partition(&mut vec, 1);
         assert_eq!(vec, [5, 2, 7, 8]);
         assert_eq!(partition_index, 2);
     }
@@ -81,8 +73,7 @@ mod tests {
     #[test]
     fn test_partition_3() {
         let mut vec = vec![5, 7, 8, 2];
-        let vec_len = vec.len() - 1;
-        let partition_index = partition(&mut vec, 2, 0, vec_len);
+        let partition_index = partition(&mut vec, 2);
         assert_eq!(vec, [5, 7, 2, 8]);
         assert_eq!(partition_index, 3);
     }
@@ -90,8 +81,7 @@ mod tests {
     #[test]
     fn test_partition_4() {
         let mut vec = vec![5, 7, 8, 2];
-        let vec_len = vec.len() - 1;
-        let partition_index = partition(&mut vec, 3, 0, vec_len);
+        let partition_index = partition(&mut vec, 3);
         assert_eq!(vec, [2, 7, 8, 5]);
         assert_eq!(partition_index, 0);
     }
@@ -99,8 +89,7 @@ mod tests {
     #[test]
     fn test_partition_5() {
         let mut vec = vec![9, 5, 7, 8, 2];
-        let vec_len = vec.len() - 1;
-        let partition_index = partition(&mut vec, 3, 0, vec_len);
+        let partition_index = partition(&mut vec, 3);
         assert_eq!(vec, [5, 7, 2, 8, 9]);
         assert_eq!(partition_index, 3);
     }
@@ -108,19 +97,73 @@ mod tests {
     #[test]
     fn test_partition_6() {
         let mut vec = vec![5, 4, 3, 2, 1];
-        let vec_len = vec.len() - 1;
-        let partition_index = partition(&mut vec, 0, 0, vec_len);
+        let partition_index = partition(&mut vec, 0);
         assert_eq!(vec, [1, 4, 3, 2, 5]);
         assert_eq!(partition_index, 4);
     }
 
     #[test]
-    fn test_quicksort() {
+    fn basic() {
+        let mut res = vec![10, 8, 4, 3, 1, 9, 2, 7, 5, 6];
+        quicksort(&mut res);
+        assert_eq!(res, vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    }
+
+    #[test]
+    fn basic_string() {
+        let mut res = vec!["a", "bb", "d", "cc"];
+        quicksort(&mut res);
+        assert_eq!(res, vec!["a", "bb", "cc", "d"]);
+    }
+
+    #[test]
+    fn empty() {
+        let mut res = Vec::<u8>::new();
+        quicksort(&mut res);
+        assert_eq!(res, vec![]);
+    }
+
+    #[test]
+    fn one_element() {
+        let mut res = vec![1];
+        quicksort(&mut res);
+        assert_eq!(res, vec![1]);
+    }
+
+    #[test]
+    fn two_elements_sorted() {
+        let mut res = vec![1, 2];
+        quicksort(&mut res);
+        assert_eq!(res, vec![1, 2]);
+    }
+
+    #[test]
+    fn two_elements_unsorted() {
+        let mut res = vec![2, 1];
+        quicksort(&mut res);
+        assert_eq!(res, vec![1, 2]);
+    }
+
+    #[test]
+    fn pre_sorted() {
+        let mut res = vec![1, 2, 3, 4];
+        quicksort(&mut res);
+        assert_eq!(res, vec![1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn reverse_sorted() {
+        let mut res = vec![4, 3, 2, 1];
+        quicksort(&mut res);
+        assert_eq!(res, vec![1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn reverse_sorted_2() {
         let mut vec = vec![5, 4, 3, 2, 1];
         quicksort(&mut vec);
         assert_eq!(vec, [1, 2, 3, 4, 5]);
     }
 
-    // FIXME add more tests for edge cases
     // FIXME add benchmark to compare this against the stdlib
 }
